1bbb5147139df677797b0440d4bea34af15526cc
kevin-home <936563422@qq.com>
2017-12-20 20:12:30 +0800

    cc

--- L_QMLBaseCompoments/main.cpp
+++ L_QMLBaseCompoments/main.cpp
@@ -7,10 +7,11 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    QString strRccPath = QGuiApplication::applicationDirPath ()+"/qml.rcc";
+//    QString strRccPath = QGuiApplication::applicationDirPath ()+"/qml.rcc";
+    QString strRccPath = "qml.rcc";
     qDebug()<<"strRccPath:"<<strRccPath;
 
-//    QByteArray baRcc = strRccPath.toLatin1 ();
+    //    QByteArray baRcc = strRccPath.toLatin1 ();
 
     QString strRoot = "";
     QResource::unregisterResource (strRccPath);
@@ -22,12 +23,12 @@ int main(int argc, char *argv[])
     {
         QResource::unregisterResource (strRccPath);
         qDebug()<<"register rcc error!";
-//        return 0;
+        //        return 0;
     }
 
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl("qrc:/main.qml"));
+    engine.load(QUrl("qrc:///main.qml"));
 
     return app.exec();
 }
